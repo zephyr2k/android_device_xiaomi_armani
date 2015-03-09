092f55447541d0037800c92cdcbebf7081ca7210
zephyr_2k <ankitnick1024@gmail.com>
2015-03-10 01:32:01 +0530

    Edit Wifi WCNSS config

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -39,7 +39,7 @@ LOCAL_MODULE       := WCNSS_qcom_cfg.ini
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
