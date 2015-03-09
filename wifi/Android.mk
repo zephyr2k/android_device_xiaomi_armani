511692e8a0cfd89888827ac1758bd56f8633cd6b
zephyr_2k <ankitnick1024@gmail.com>
2015-03-09 23:54:51 +0530

    Wifi woes

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -39,7 +39,7 @@ LOCAL_MODULE       := WCNSS_qcom_cfg.ini
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -54,7 +54,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := wlan_module_symlink
 LOCAL_MODULE_CLASS := FAKE
-
 include $(BUILD_SYSTEM)/base_rules.mk
 
 $(LOCAL_BUILT_MODULE): WLAN_MODULE := /system/lib/modules/pronto/pronto_wlan.ko
